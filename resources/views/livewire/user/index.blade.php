f55c24c090cebf21e595b48c7110c9014df9488f
devryank <ryansg41@gmail.com>
2021-06-18 18:43:39 +0700

    fix alert message

--- resources/views/livewire/user/index.blade.php
+++ resources/views/livewire/user/index.blade.php
@@ -15,12 +15,12 @@
 
     @if (session()->has('message'))
     {{-- alert --}}
-    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-green-500 alert">
+    <div class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-{{session('color')}}-500 alert">
         <span class="text-xl inline-block mr-5 align-middle">
             <i class="fas fa-check"></i>
         </span>
         <span class="inline-block align-middle mr-8">
-            test
+            {{session('message')}}
         </span>
         <button class="absolute bg-transparent text-2xl font-semibold leading-none right-0 top-0 mt-4 mr-6 outline-none focus:outline-none"
                 onclick="closeAlert(event)">
