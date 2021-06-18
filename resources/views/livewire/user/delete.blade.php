e9351b658eb01de17bbb313573db8512e5330258
devryank <ryansg41@gmail.com>
2021-06-18 17:53:17 +0700

    fix emit close handler

--- resources/views/livewire/user/delete.blade.php
+++ resources/views/livewire/user/delete.blade.php
@@ -10,7 +10,7 @@
                     <button wire:click="destroyUser"
                             class="px-4 py-1 text-white font-light tracking-wider bg-red-700 rounded"
                             type="submit">Delete</button>
-                    <button wire:click="$emit('closeDeleteUser')"
+                    <button wire:click="$emit('closeUser')"
                             class="px-4 py-1 text-white font-light tracking-wider bg-gray-900 rounded">Cancel</button>
                 </div>
             </div>
