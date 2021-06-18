d1fbc7818eb5e014d6b559bf80ef7a618016e135
devryank <ryansg41@gmail.com>
2021-06-18 18:43:56 +0700

    remove icon add user

--- resources/views/livewire/user/create.blade.php
+++ resources/views/livewire/user/create.blade.php
@@ -2,7 +2,7 @@
     <div class="flex-flex-wrap">
         <div class="w-full my-6 pr-0 lg:pr-2">
             <p class="text-xl pb-6 flex items-center">
-                <i class="fas fa-list mr-3"></i> Add User
+                Add User
             </p>
             <div class="leading-loose">
                 <form wire:submit.prevent="store"
